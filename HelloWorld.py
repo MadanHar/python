dc5ec3cc68c9c401a8c6c91ba698fa55c84f343c
Madan Haraginadoni <madan.haraginadoni@auspost.com.au>
2019-01-29 11:19:39 +1100

    Initial commit.

---
+++ HelloWorld.py
@@ -0,0 +1,10 @@
+
+greeting = "Hello"
+#name = input("Please enter your name ")
+#print(greeting +" " + name)
+
+splitString = "This string has been \n split over \n several\nlines"
+print(splitString)
+
+today = "friday"
+print("fri" in today)
